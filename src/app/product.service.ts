7072439ab68d151481ee0d61615ada800f472ebf
hrouby <33086625+hrouby@users.noreply.github.com>
2020-06-11 15:21:04 +0200

    second step v3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -3,8 +3,9 @@ import { Http, Response} from '@angular/http'
 @Injectable()
 export class ProductService {
 
-  constructor(private _http: Http, private _albumUrl: '../assets/album.json') {
+  constructor(private _http: Http) {
 
   }
+  {}
 
 }
