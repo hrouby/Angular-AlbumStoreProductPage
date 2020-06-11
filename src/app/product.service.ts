69daa97ed84df50f955a3a63bb993485150a49bb
hrouby <33086625+hrouby@users.noreply.github.com>
2020-06-11 16:06:14 +0200

    3step V2

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -11,7 +11,7 @@ export class ProductService {
 
   }
   getAlbum(id: number): Observable<Album> {
-    return this._http.get(this._albumUrl).map((response) => (response.json() as Album));
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json() );
   }
 
 }
